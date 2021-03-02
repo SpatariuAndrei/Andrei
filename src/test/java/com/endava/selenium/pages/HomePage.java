ea99356a2ba58d458b81d4fa14349757bbfe1349
Toader Mihali <toader.mihali@fisglobal.com>
2021-03-02 15:03:35 +0200

    add file logger

--- src/test/java/com/endava/selenium/pages/HomePage.java
+++ src/test/java/com/endava/selenium/pages/HomePage.java
@@ -3,10 +3,17 @@ package com.endava.selenium.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HomePage implements BasePage {
+
+    /**
+     * Logger
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomePage.class);
 
     @FindBy(how = How.CSS, using = "#search_form_input_homepage")
     private WebElement searchInput;
@@ -15,10 +22,12 @@ public class HomePage implements BasePage {
     private WebElement searchButton;
 
     public void inputSearch(String search){
+        LOGGER.info("Input search query: " + search + " using locator: " + searchInput.toString());
         searchInput.sendKeys(search);
     }
 
     public void pressSearchButton(){
+        LOGGER.info("Clicking on search button using locator: " + searchButton.toString());
         searchButton.click();
     }
 }
